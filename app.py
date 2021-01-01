30a2b7db916a849e4b2de8e8902a5756ae1e3cdb
NovaDSA <info@novadsa.com>
2021-01-01 22:31:01 +0300

    Fix

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/": {"origins": "*"}})
 
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
 
 api = Api(app)
 
 api.add_resource(Products, "/products")
-api.add_resource(Product,"/product")
+api.add_resource(Product, "/product")
 
 if __name__ == "__main__":
     app.run(debug=True)
