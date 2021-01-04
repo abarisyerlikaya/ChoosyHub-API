9df9fa1fe45aa1d8a51a6d72a9fe1a7c63c24c13
NovaDSA <info@novadsa.com>
2021-01-04 23:21:58 +0300

    Filters

--- apis.py
+++ apis.py
@@ -2,6 +2,7 @@ from flask_restful import Resource, request
 from flask.json import jsonify
 from pymongo import MongoClient
 from re import I, compile
+from math import ceil
 
 
 class Products(Resource):
@@ -12,14 +13,48 @@ class Products(Resource):
         self.collection = self.db["products"]
 
     def get(self):
-        name = request.args["name"]
-        page = int(request.args["page"])
+        key = request.args.get("key") if request.args.get(
+            "key") is not None else ""
+        page = int(request.args.get("page")) if request.args.get(
+            "page") is not None else 1
 
-        expression = compile(f'.*{name}.*', I)
+        expression = compile(f'.*{key}.*', I)
 
-        cursor = self.collection.find({"name": {"$regex": expression}}, {
-                                      "_id": 1, "name": 1, "price": 1, "rating": 1, "number_of_reviews": 1, "number_of_comments": 1, "pictures": 1})
-        page_count = cursor.count() // 10 + 1
+        query = {"name": {"$regex": expression}}
+        returning_fields = {"_id": 1, "name": 1, "price": 1, "rating": 1,
+                            "number_of_reviews": 1, "number_of_comments": 1, "pictures": 1}
+
+        if request.args.get("minPrice") is not None and request.args.get("maxPrice") is not None:
+            query["price"] = {"$gte": int(request.args.get("minPrice")), "$lte": int(request.args.get("maxPrice")) }
+        elif request.args.get("minPrice") is not None:
+            query["price"] = {"$gte": int(request.args.get("minPrice"))}
+        elif request.args.get("maxPrice") is not None:
+            query["price"] = {"$lte": int(request.args.get("maxPrice"))}
+        if request.args.get("minRating") is not None:
+            query["rating"] = {"$gte": int(request.args.get("minRating"))}
+        if request.args.get("minReviewCount") is not None:
+            query["number_of_reviews"] = {
+                "$gte": int(request.args.get("minReviewCount"))}
+        if request.args.get("minCommentCount") is not None:
+            query["number_of_comments"] = {
+                "$gte": int(request.args.get("minCommentCount"))}
+
+        sort_by = [("number_of_reviews", -1)]
+
+        if request.args.get("sortBy") is not None:
+            if request.args.get("sortBy") == "numberOfReviews":
+                sort_by = [("number_of_reviews", -1)]
+            elif request.args.get("sortBy") == "numberOfComments":
+                sort_by = [("number_of_comments", -1)]
+            elif request.args.get("sortBy") == "rating":
+                sort_by = [("rating", -1)]
+            elif request.args.get("sortBy") == "priceAsc":
+                sort_by = [("price", 1)]
+            elif request.args.get("sortBy") == "priceDsc":
+                sort_by = [("price", -1)]
+
+        cursor = self.collection.find(query, returning_fields).sort(sort_by)
+        page_count = ceil(cursor.count() / 10)
 
         response = {"meta": {"currentPage": page, "pageCount": page_count},
                     "body": [doc for doc in cursor.skip((page-1)*10).limit(10)]}
--- app.py
+++ app.py
@@ -13,9 +13,9 @@ cors = CORS(app, resources={r"/": {"origins": "*"}})
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     response.headers.add('Access-Control-Allow-Headers',
                          'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
 
